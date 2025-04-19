fad405e8db36f72317f7dffb6301d6bde26d892c
Mathieu Moalic <matmoa@pm.me>
2025-04-19 17:36:45 +0200

    feat: 2015/01

--- src/year2015/day01.rs
+++ src/year2015/day01.rs
@@ -7,8 +7,28 @@ pub fn run(part: usize, input: String) -> String {
 }
 
 pub fn part1(input: String) -> String {
-    input.to_owned()
+    let mut floor: isize = 0;
+    for c in input.chars() {
+        if c == '(' {
+            floor += 1;
+        } else {
+            floor -= 1;
+        }
+    }
+    floor.to_string()
 }
+
 pub fn part2(input: String) -> String {
-    input.to_owned()
+    let mut floor: isize = 0;
+    for (i, c) in input.chars().enumerate() {
+        if c == '(' {
+            floor += 1;
+        } else {
+            floor -= 1;
+        }
+        if floor < 0 {
+            return (i + 1).to_string();
+        }
+    }
+    String::from("Something went wrong")
 }
