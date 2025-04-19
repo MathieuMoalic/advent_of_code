45052b3efeef32e6b6bb136bcbaeb8820a240876
Mathieu Moalic <matmoa@pm.me>
2025-04-19 19:04:22 +0200

    feat: 2015/03

--- src/year2015/day03.rs
+++ src/year2015/day03.rs
@@ -7,8 +7,50 @@ pub fn run(part: usize, input: String) -> String {
 }
 
 pub fn part1(input: String) -> String {
-    input.to_owned()
+    let mut x: usize = 256;
+    let mut y: usize = 256;
+    let mut grid: Vec<Vec<usize>> = vec![vec![0; 512]; 512];
+    grid[256][256] = 1;
+    for c in input.chars() {
+        match c {
+            '<' => x -= 1,
+            '>' => x += 1,
+            'v' => y -= 1,
+            '^' => y += 1,
+            _ => panic!("wtf"),
+        }
+        grid[y][x] += 1
+    }
+
+    let non_zero_count = grid.iter().flatten().filter(|&&val| val != 0).count();
+    non_zero_count.to_string()
 }
+
+#[derive(PartialEq, Clone, Copy)]
+struct Santa {
+    x: usize,
+    y: usize,
+}
+
 pub fn part2(input: String) -> String {
-    input.to_owned()
+    let mut santa = Santa { x: 256, y: 256 };
+    let mut robot = Santa { x: 256, y: 256 };
+    let mut grid: Vec<Vec<usize>> = vec![vec![0; 512]; 512];
+    grid[256][256] = 2;
+    for (i, c) in input.chars().enumerate() {
+        let active = if i % 2 == 0 { &mut santa } else { &mut robot };
+
+        match c {
+            '<' => active.x -= 1,
+            '>' => active.x += 1,
+            'v' => active.y -= 1,
+            '^' => active.y += 1,
+            _ => panic!("Invalid input"),
+        }
+
+        grid[active.y][active.x] += 1;
+    }
+
+    let non_zero_count = grid.iter().flatten().filter(|&&val| val != 0).count();
+    non_zero_count.to_string()
 }
