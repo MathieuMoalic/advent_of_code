99754e14ba1872118db60cd3a731a614807a03d1
Mathieu Moalic <matmoa@pm.me>
2025-04-19 18:21:27 +0200

    feat: 2015/02

--- src/year2015/day02.rs
+++ src/year2015/day02.rs
@@ -7,8 +7,30 @@ pub fn run(part: usize, input: String) -> String {
 }
 
 pub fn part1(input: String) -> String {
-    input.to_owned()
+    let mut total: usize = 0;
+    for line in input.lines() {
+        let dims: Vec<usize> = line
+            .split('x')
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
+        let (h, l, w) = (dims[0], dims[1], dims[2]);
+        let sides = [h * l, w * l, h * w];
+        total += sides.iter().sum::<usize>() * 2;
+        total += *sides.iter().min().unwrap();
+    }
+    total.to_string()
 }
+
 pub fn part2(input: String) -> String {
-    input.to_owned()
+    let mut total: usize = 0;
+    for line in input.lines() {
+        let mut dims: Vec<usize> = line
+            .split('x')
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
+        dims.sort();
+        total += dims[0] * 2 + dims[1] * 2;
+        total += dims.iter().product::<usize>();
+    }
+    total.to_string()
 }
