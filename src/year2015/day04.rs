b7dffb80b70bf863bc2e84687a92bbb11cf16f42
Mathieu Moalic <matmoa@pm.me>
2025-04-20 15:13:47 +0200

    feat: 2015/04

--- src/year2015/day04.rs
+++ src/year2015/day04.rs
@@ -7,8 +7,41 @@ pub fn run(part: usize, input: String) -> String {
 }
 
 pub fn part1(input: String) -> String {
-    input.to_owned()
+    let mut str_to_hash = String::with_capacity(input.len() + 20);
+    for i in 0..usize::MAX {
+        str_to_hash.clear();
+        str_to_hash.push_str(&input);
+        str_to_hash.push_str(&i.to_string());
+        let hash = md5::compute(&str_to_hash).0;
+
+        // hash[0] => bits 0..8
+        // hash[1] => bits 8..16
+        // hash[2] => bits 16..24
+        if hash[0] == 0 && hash[1] == 0 && (hash[2] >> 4) == 0 {
+            return i.to_string();
+        }
+    }
+    panic!("Couldnt find answer")
 }
+
+use std::fmt::Write;
 pub fn part2(input: String) -> String {
-    input.to_owned()
+    let mut str_to_hash = String::with_capacity(input.len() + 20);
+    let mut num_buf = String::with_capacity(20);
+
+    for i in 0..usize::MAX {
+        str_to_hash.clear();
+        str_to_hash.push_str(&input);
+
+        num_buf.clear();
+        write!(&mut num_buf, "{}", i).unwrap();
+        str_to_hash.push_str(&num_buf);
+
+        let hash = md5::compute(&str_to_hash).0;
+
+        if hash[0] == 0 && hash[1] == 0 && hash[2] == 0 {
+            return i.to_string();
+        }
+    }
+    panic!("Couldnt find answer")
 }
