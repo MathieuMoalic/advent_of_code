2c46396cff0bfec522beea479d8f47cd792250af
Mathieu Moalic <matmoa@pm.me>
2025-04-21 14:03:58 +0200

    feat: 2015/10

--- src/year2015/day10.rs
+++ src/year2015/day10.rs
@@ -7,8 +7,49 @@ pub fn run(part: usize, input: String) -> String {
 }
 
 pub fn part1(input: String) -> String {
-    input.to_owned()
+    let mut digits: Vec<u32> = input.chars().map(|c| c.to_digit(10).unwrap()).collect();
+    for _ in 0..40 {
+        let mut new_digits: Vec<u32> = Vec::new();
+        let mut last_digit = digits[0];
+        let mut digit_count = 1;
+        (1..digits.len()).for_each(|i| {
+            let d = digits[i];
+            if d == last_digit {
+                digit_count += 1
+            } else {
+                new_digits.push(digit_count);
+                new_digits.push(last_digit);
+                last_digit = d;
+                digit_count = 1;
+            }
+        });
+        new_digits.push(digit_count);
+        new_digits.push(last_digit);
+        digits = new_digits;
+    }
+    digits.len().to_string()
 }
+
 pub fn part2(input: String) -> String {
-    input.to_owned()
+    let mut digits: Vec<u32> = input.chars().map(|c| c.to_digit(10).unwrap()).collect();
+    for _ in 0..50 {
+        let mut new_digits: Vec<u32> = Vec::new();
+        let mut last_digit = digits[0];
+        let mut digit_count = 1;
+        (1..digits.len()).for_each(|i| {
+            let d = digits[i];
+            if d == last_digit {
+                digit_count += 1
+            } else {
+                new_digits.push(digit_count);
+                new_digits.push(last_digit);
+                last_digit = d;
+                digit_count = 1;
+            }
+        });
+        new_digits.push(digit_count);
+        new_digits.push(last_digit);
+        digits = new_digits;
+    }
+    digits.len().to_string()
 }
