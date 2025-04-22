c84e65da176fa157aa7e22b3ae8414aac4d08b08
Mathieu Moalic <matmoa@pm.me>
2025-04-22 21:56:34 +0200

    feat: 2015/12

--- src/year2015/day12.rs
+++ src/year2015/day12.rs
@@ -6,9 +6,46 @@ pub fn run(part: usize, input: String) -> String {
     }
 }
 
+use regex::Regex;
 pub fn part1(input: String) -> String {
-    input.to_owned()
+    let re = Regex::new(r"-?[1-9][0-9]*").unwrap();
+    re.find_iter(&input)
+        .map(|x| x.as_str().parse::<i32>().unwrap())
+        .collect::<Vec<i32>>()
+        .into_iter()
+        .sum::<i32>()
+        .to_string()
 }
+
+use serde_json::Value;
+
+fn walk_json(value: &Value, count: &mut i32) {
+    match value {
+        Value::Object(map) => {
+            if !map
+                .values()
+                .any(|v| matches!(v, Value::String(s) if s == "red"))
+            {
+                for (_, val) in map {
+                    walk_json(val, count);
+                }
+            }
+        }
+        Value::Array(arr) => {
+            for val in arr.iter() {
+                walk_json(val, count);
+            }
+        }
+        Value::Number(n) => {
+            *count += n.as_i64().unwrap() as i32;
+        }
+        _ => {}
+    }
+}
+
 pub fn part2(input: String) -> String {
-    input.to_owned()
+    let mut count: i32 = 0;
+    let v: Value = serde_json::from_str(&input).unwrap();
+    walk_json(&v, &mut count);
+    count.to_string()
 }
