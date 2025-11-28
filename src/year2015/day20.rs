99aa4291ed659f8bb022acac0e4aba4d0493bc8a
Mathieu Moalic <matmoa@pm.me>
2025-11-28 14:31:16 +0100

    feat: 2015/20

--- src/year2015/day20.rs
+++ src/year2015/day20.rs
@@ -5,10 +5,48 @@ pub fn run(part: usize, input: String) -> String {
         _ => panic!("Invalid part: {}", part),
     }
 }
+const MAX_HOUSE: usize = 1_000_000;
+const MAX_VISITS: usize = 50;
 
 pub fn part1(input: String) -> String {
-    input.to_owned()
+    let goal = input.parse::<usize>().unwrap() / 10;
+    let mut houses = vec![0usize; MAX_HOUSE + 1];
+
+    for elf in 1..=MAX_HOUSE {
+        for house in (elf..=MAX_HOUSE).step_by(elf) {
+            houses[house] += elf;
+        }
+    }
+
+    houses
+        .iter()
+        .enumerate()
+        .skip(1) // ignore fake house 0
+        .find(|&(_house, &presents)| presents >= goal)
+        .map(|(house, _)| house)
+        .unwrap()
+        .to_string()
 }
+
 pub fn part2(input: String) -> String {
-    input.to_owned()
+    let goal = input.parse::<usize>().unwrap();
+    let mut houses = vec![0usize; MAX_HOUSE + 1];
+
+    for elf in 1..=MAX_HOUSE {
+        for (visits, house) in (elf..=MAX_HOUSE).step_by(elf).enumerate() {
+            if visits == MAX_VISITS {
+                break;
+            }
+            houses[house] += elf * 11;
+        }
+    }
+
+    houses
+        .iter()
+        .enumerate()
+        .skip(1) // ignore fake house 0
+        .find(|&(_house, &presents)| presents >= goal)
+        .map(|(house, _)| house)
+        .unwrap()
+        .to_string()
 }
